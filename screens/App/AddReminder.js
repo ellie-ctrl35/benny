549958bb004c8a95771280562b419a9c651b1d51
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2024-04-02 01:12:42 +0000

    Addreminder Screen

---
+++ screens/App/AddReminder.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function AddReminder() {
+  return (
+    <View>
+      <Text>AddReminder</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
