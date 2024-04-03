34096ace65c6e4fd3d8c44df66cb951ae41b891b
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2024-04-03 00:17:20 +0000

    premiuim box refactor

--- screens/App/HomeScreen.js
+++ screens/App/HomeScreen.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView,SafeAreaView } from 'react-native';
-import { AntDesign, MaterialCommunityIcons } from '@expo/vector-icons';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  SafeAreaView,
+} from "react-native";
+import { AntDesign, MaterialCommunityIcons } from "@expo/vector-icons";
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerTitle}>Premium Plan</Text>
-        <Text style={styles.headerSubtitle}>Harness the full power of AI with a Premium Plan</Text>
-        <TouchableOpacity style={styles.upgradeButton}>
-            
-          <Text style={styles.upgradeButtonText}>Upgrade now</Text>
-        </TouchableOpacity>
-      </View>
       <View style={styles.featureContainer}>
         <View style={styles.featureBox}>
-        <MaterialCommunityIcons name="robot-angry" size={24} color="#4b9eb2" />
-          <Text style={styles.featureText}>Ask our AI Assistant about anuthing Health related</Text>
+          <Text style={styles.featureText}>Completed</Text>
+          <Text style={styles.featureText}>
+            Ask our AI Assistant about anuthing Health related
+          </Text>
         </View>
         <View style={styles.featureBox}>
-        <MaterialCommunityIcons name="alarm" size={24} color="#4b9eb2" />
-          <Text style={styles.featureText}>Set Reminders for your Medicine </Text>
+        <Text style={styles.featureText}>Scheduled</Text>
+          <Text style={styles.featureText}>
+            Set Reminders for your Medicine{" "}
+          </Text>
+        </View>
+      </View>
+
+      <View style={styles.featureContainer}>
+        <View style={styles.featureBox}>
+          <MaterialCommunityIcons
+            name="robot-angry"
+            size={24}
+            color="#4b9eb2"
+          />
+          <Text style={styles.featureText}>
+            Ask our AI Assistant about anuthing Health related
+          </Text>
+        </View>
+        <View style={styles.featureBox}>
+          <MaterialCommunityIcons name="alarm" size={24} color="#4b9eb2" />
+          <Text style={styles.featureText}>
+            Set Reminders for your Medicine{" "}
+          </Text>
         </View>
       </View>
       <Text style={styles.historyTitle}>History</Text>
@@ -37,57 +60,57 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#191919', // Assuming the background is black
+    backgroundColor: "#191919", // Assuming the background is black
   },
   header: {
     padding: 20,
-    backgroundColor: '#5ad7ff', // Dark background for header
+    backgroundColor: "#5ad7ff", // Dark background for header
     borderRadius: 20,
-    marginTop:'2%',
-    marginHorizontal:'3%',
+    marginTop: "2%",
+    marginHorizontal: "3%",
   },
   headerTitle: {
-    color: '#000',
-    fontWeight: 'bold',
+    color: "#000",
+    fontWeight: "bold",
     fontSize: 20,
   },
   headerSubtitle: {
-    color: '#000',
+    color: "#000",
     fontSize: 16,
     marginBottom: 10,
   },
   upgradeButton: {
-    backgroundColor: '#000', // Example green color
+    backgroundColor: "#000", // Example green color
     padding: 10,
     borderRadius: 5,
-    alignItems: 'center',
+    alignItems: "center",
   },
   upgradeButtonText: {
-    color: '#fff',
-    fontWeight: 'bold',
+    color: "#fff",
+    fontWeight: "bold",
   },
   featureContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    width: '100%',
-    marginTop:'3%'
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    width: "100%",
+    marginTop: "3%",
   },
   featureBox: {
-    backgroundColor: '#222729', // Dark boxes for features
+    backgroundColor: "#222729", // Dark boxes for features
     padding: 15,
     borderRadius: 10,
-    alignItems: 'center',
-    width: '45%',
+    alignItems: "center",
+    width: "45%",
   },
   featureText: {
-    color: '#fff',
+    color: "#fff",
     marginTop: 5,
   },
   historyTitle: {
-    color: '#fff',
+    color: "#fff",
     padding: 20,
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   historyContainer: {
     // styles for history container
